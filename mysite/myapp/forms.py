429cde8e7bcbc7e4d09167029682a735e3370415
woorim1022 <woorim1022@naver.com>
2020-01-20 23:59:23 +0900

    ㅇㅇ

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -5,7 +5,7 @@ from emoji_picker.widgets import EmojiPickerTextInputAdmin, EmojiPickerTextareaA
 class LetterForm(forms.ModelForm): # PostForm이라는 이름의 모델폼 클래스 생성
     class Meta:
         model = Letter # form에서 사용할 모델이 Post임을 명시
-        fields = ['title','body','bgm']
+        fields = ['title','body']
 
 class CommentForm(forms.ModelForm):
     class Meta:
